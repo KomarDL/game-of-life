22042dd1b3d725c02b12f92e3b53a3f1f6c16c14
KomarDL <deagl_Dim@mail.ru>
2021-06-05 22:21:40 +0300

    add: cell coloring using mouse
    add: using for cell ptr
    add: handling of mouse events
    change: now default cell color is white

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,11 +1,18 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QColorDialog>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QColorDialog *colorDialog = new QColorDialog(this);
+//    colorDialog->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    ui->gridLayout->addWidget(colorDialog, 0, 1);
+    colorDialog->setWindowFlags(Qt::SubWindow);
+    /* a few options that we must set for it to work nicely */
+    colorDialog->setOptions(QColorDialog::DontUseNativeDialog | QColorDialog::NoButtons);
 }
 
 MainWindow::~MainWindow()
