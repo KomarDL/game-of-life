4330d9cd901a0905fa001f8583801ea1bd0248b3
KomarDL <deagl_Dim@mail.ru>
2021-06-05 14:56:49 +0300

    add: creation of the game field

--- src/gamefield.cpp
+++ src/gamefield.cpp
@@ -1,6 +1,65 @@
 #include "gamefield.h"
+#include <QPaintEvent>
 
-GameField::GameField(QWidget *parent) : QWidget(parent)
+GameField::GameField(QWidget *parent) : GameField(DEFAULT_SIZE, parent)
 {
 
+}
+
+GameField::GameField(const QSize &fieldSize, QWidget *parent) : QWidget(parent)
+    , m_fieldSize(fieldSize)
+{
+    auto palette = this->palette();
+    palette.setBrush(QPalette::Window, Qt::white);
+    setPalette(palette);
+    setAutoFillBackground(true);
+}
+
+void GameField::setFieldSize(QSize fieldSize)
+{
+    if (m_fieldSize == fieldSize) {
+        return;
+    }
+    m_fieldSize = fieldSize;
+    createField();
+    emit fieldSizeChanged(m_fieldSize);
+}
+
+void GameField::paintEvent(QPaintEvent *event)
+{
+
+}
+
+void GameField::createField()
+{
+    resizeField();
+    auto rowRect = generateInitialRect();
+    for (auto i = 0; i < m_field.size(); ++i) {
+        auto& row = m_field[i];
+        rowRect.moveBottom(rowRect.bottom() + rowRect.height());
+        auto columnRect = rowRect;
+        for (auto j = 0; j < row.size(); ++j) {
+            auto& cell = row[j];
+            columnRect.moveRight(columnRect.right() + columnRect.width());
+            if (cell == nullptr) {
+                cell = std::make_shared<Cell>();
+            }
+            cell->setRect(columnRect);
+        }
+    }
+}
+
+void GameField::resizeField()
+{
+    m_field.resize(m_fieldSize.height());
+    for (auto& row : m_field) {
+        row.resize(m_fieldSize.width());
+    }
+}
+
+QRect GameField::generateInitialRect()
+{
+    auto rectWidth = this->width() / m_fieldSize.width();
+    auto rectHeigth = this->height() / m_fieldSize.height();
+    return QRect(0, 0, rectWidth, rectHeigth);
 }
--- src/gamefield.h
+++ src/gamefield.h
@@ -1,19 +1,40 @@
 #ifndef GAMEFIELD_H
 #define GAMEFIELD_H
 
+#include <memory>
+
 #include <QWidget>
+#include <QVector>
+#include <QRect>
+
 #include "cell.h"
 
-class GameField : public QWidget
-{
+class GameField : public QWidget {
     Q_OBJECT
 public:
-    explicit GameField(QWidget *parent = nullptr);
+    using row = QVector<std::shared_ptr<Cell>>;
+    static constexpr QSize DEFAULT_SIZE{10, 10};
+
+    explicit GameField(QWidget* parent = nullptr);
+    explicit GameField(const QSize& fieldSize = DEFAULT_SIZE, QWidget* parent = nullptr);
+
+    QSize fieldSize() const noexcept { return m_fieldSize; }
 
 Q_SIGNALS:
+    void fieldSizeChanged(QSize fieldSize);
+public Q_SLOTS:
+    void setFieldSize(QSize fieldSize);
+
+protected:
+    void paintEvent(QPaintEvent *event) override;
 
 private:
+    QSize m_fieldSize;
+    QVector<row> m_field;
 
+    void createField();
+    void resizeField();
+    QRect generateInitialRect();
 };
 
 #endif // GAMEFIELD_H
